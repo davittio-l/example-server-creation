b5fc3bcd5ea6d4606fb737dcb107c35a568f17fa
Colten <129473270+Davittio-L@users.noreply.github.com>
2024-05-14 20:55:11 -0400

    added messages endpoint with html.

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
+    if (req.url === '/friends') {
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/json',
+    // });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({
         id:1,
         name: 'James Bond'
     }));
+    } else if (req.url === '/messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello james!<li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
