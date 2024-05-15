4e30679cea1849d05ae3515724a19f86a4bcf8ad
Colten <129473270+Davittio-L@users.noreply.github.com>
2024-05-14 21:06:09 -0400

    created peramertized route for friends.

--- index.js
+++ index.js
@@ -2,23 +2,40 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 0,
+        name: 'James',
+        age: 25,
+        hobbies: ['music', 'coding', 'gaming'],
+    },
+    {
+        id: 1,
+        name: 'Jane',
+        age: 23,
+        hobbies: ['music', 'coding', 'gaming'],
+    },
+    {
+        id: 2,
+        name: 'John',
+        age: 22,
+        hobbies: ['music', 'coding', 'gaming'],
+    },
+];
+
+server.on('request', (req, res) => {
     if (req.url === '/friends') {
-    // res.writeHead(200, {
-    //     'Content-Type': 'application/json',
-    // });
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        id:1,
-        name: 'James Bond'
-    }));
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(friends));
     } else if (req.url === '/messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
         res.write('<ul>');
-        res.write('<li>Hello james!<li>');
+        res.write('<li>Hello James!</li>');  // Corrected the closing <li> tag
         res.write('<li>What are your thoughts on astronomy?</li>');
         res.write('</ul>');
         res.write('</body>');
